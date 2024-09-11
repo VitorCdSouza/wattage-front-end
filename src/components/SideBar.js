3c511f611a8239bee14193852fa1e48bdeab2a5b
VitorCdSouza <vitorcorddesouza@gmail.com>
2024-09-10 23:31:44 -0300

    continuing sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,18 +1,27 @@
 import { MdOutlineBedroomChild } from "react-icons/md";
+import "./SideBar.css"
 
 const SideBar = () => {
     return (
-        <div className="fixed top-0 left-0 h-screen w-80 m-0
-                        flex flex-col bg-gray-900  text-white shadow-lg">
+        <div className="fixed top-0 left-0 h-screen w-72 m-0
+                        flex flex-col bg-gray-900 text-white shadow-lg font-serif">
+            
+            <div className="flex mt-5 ms-3 mb-6">
+                <img className="size-20" src="/images/wm-favicon-color.svg" alt="logo" />
+                <h1 className="mt-4 text-5xl">WM</h1>
+            </div>
 
+            <SideBarItem icon={<MdOutlineBedroomChild size={40} />} name="Rooms"/>
+            <SideBarItem icon={<MdOutlineBedroomChild size={40} />} name="Devices"/>
+            <SideBarItem icon={<MdOutlineBedroomChild size={40} />} />
             <SideBarItem icon={<MdOutlineBedroomChild size={40} />} />            
         </div>
     )
 }
 
-const SideBarItem = ({ icon }) => (
-    <div className="sidebar-item flex mt-10 ml-6">
-        {icon} <p className='ml-2'>asdas</p>
+const SideBarItem = (props) => (
+    <div className="sidebar-item flex mt-7 ms-6">
+        {props.icon} <p className='ms-2 mt-1.5 text-xl'>{props.name}</p>
     </div>
 )
 
